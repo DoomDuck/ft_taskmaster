6adefdb6168c1b43d5e1f74854b085b827e13937
DoomDuck <docdoomduck@protonmail.com>
2024-10-16 16:20:26 +0200

    feat: Basic task management

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
-import datetime
 from enum import Enum
+from typing import Optional
+from datetime import timedelta
+from dataclasses import dataclass, field
 
 
 class RestartType(Enum):
@@ -8,27 +10,16 @@ class RestartType(Enum):
     ONFAILURE = 3
 
 
-class Duration:
-    pass
+def default_success_exit_codes() -> list[int]:
+    return [0]
 
 
 class Signal:
     pass
 
 
-class Path(str):
-    pass
-
-
-class Environment:
-    pass
-
-
-class Umask:
-    pass
-
-
-class Configuration:
+@dataclass
+class TaskDescription:
     """
     - Start command
     - Number of process to start and keep alive
@@ -48,21 +39,37 @@ class Configuration:
     - The umask used by the program
     """
 
-    command: list[str]
-    process_count: int
-    start_on_launch: bool
-    restart: RestartType
-    success_exit_codes: list[int]
-    success_start_delay: datetime.timedelta
-    restart_attempts: int
-    gracefull_shutdown_signal: Signal
-    gracefull_shutdown_success_delay: datetime.timedelta
-    stdout: Path | None
-    stderr: Path | None
-    environment: Environment
-    pwd: Path
-    umask: Umask
+    command: str
+    replicas: int = 1
+    start_on_launch: bool = True
+    restart: RestartType = RestartType.ONFAILURE
+    success_exit_codes: list[int] = \
+        field(default_factory=default_success_exit_codes)
+    success_start_delay: timedelta = timedelta(seconds=3)
+    restart_attempts: int = 3
+    # TODO(Dorian): Make the `Signal` class
+    gracefull_shutdown_signal: Optional[Signal] = None
+    gracefull_shutdown_success_delay: timedelta = timedelta(seconds=3)
+    stdout: Optional[str] = None
+    stderr: Optional[str] = None
+    environment: dict[str, str] = field(default_factory=dict)
+    pwd: Optional[str] = None
+    umask: Optional[int] = None
 
 
-def parse() -> Configuration:
-    raise "Unimplemented"
+@dataclass
+class Configuration:
+    tasks: dict[str, TaskDescription]
+
+
+def example() -> Configuration:
+    return Configuration({
+        "sleep": TaskDescription("sleep 10s"),
+        "crasher": TaskDescription("./scripts/random_crash.sh"),
+        # "hello": TaskDescription("echo hello"),
+        # "bye": TaskDescription("echo bye"),
+    })
+
+
+def parse() -> TaskDescription:
+    raise Exception("Unimplemented")
---
+++ src/runner.py
@@ -0,0 +1,142 @@
+import asyncio
+import os
+from asyncio.subprocess import Process
+from enum import Enum
+from typing import Optional, Any
+from config import Configuration, TaskDescription
+import logging
+
+
+class Status(Enum):
+    # Process is stop or has'nt been started
+    STOPPED = 0
+    # Process hasn't been running long enough to be considered
+    # completly successfully started
+    STARTING = 1
+    # Process in running
+    RUNNING = 2
+    # Process has been started several times without success
+    BACKOFF = 3
+    # Process has received its stop signal but isn't done yet
+    STOPPING = 4
+    # Process has exited from running state
+    EXITED = 5
+    # Process could not start running
+    FATAL = 6
+
+
+class Task:
+    name: str
+    status: Status
+    description: TaskDescription
+    process: Optional[Process]
+    # TODO: Add logger
+
+    def __init__(self, name: str, description: TaskDescription):
+        self.name = name
+        self.description = description
+        self.status = Status.STOPPED
+        self.processes = None
+
+    def make_start_arguments(self) -> dict[str, Any]:
+        arguments: dict[str, Any] = {}
+        if self.description.environment is not None:
+            env = os.environ.copy()
+            env.update(self.description.environment)
+            arguments["env"] = env
+
+        if self.description.pwd is not None:
+            arguments["pwd"] = self.description.pwd
+
+        return arguments
+
+    async def start(self):
+        self.status = Status.STARTING
+        logging.info(f"{self.name}: Starting")
+        self.process = await asyncio.create_subprocess_shell(
+                self.description.command,
+                **self.make_start_arguments())
+        await self.wait_successful_start()
+
+    async def wait_successful_start(self):
+        logging.info(f"{self.name}: Waiting for successful start")
+        delay = self.description.success_start_delay.total_seconds()
+        try:
+            async with asyncio.timeout(delay):
+                await self.wait()
+                logging.info(f"{self.name}: Start failed")
+                raise TaskStartFailure(self.description)
+        except asyncio.TimeoutError:
+            logging.info(f"{self.name}: Successful start")
+            self.status = Status.RUNNING
+
+    # TODO: Add gracefull stop && force stop methods
+    async def wait(self) -> Optional[int]:
+        if self.process is None:
+            return None
+        exit_code = await self.process.wait()
+        self.process = None
+        self.status = Status.STOPPED
+        return exit_code
+
+    async def start_until_success(self):
+        logging.info(f"{self.name}: Starting task")
+        for i in range(self.description.restart_attempts):
+            logging.info(f"{self.name}: Start number {i + 1}")
+            try:
+                return await self.start()
+            except TaskStartFailure:
+                # TODO: log error
+                continue
+            except Exception as e:
+                logging.info(f"{self.name}: Error starting process {e}")
+                self.status = Status.FATAL
+                return
+        logging.info(f"{self.name}: Could not start process")
+        self.status = Status.BACKOFF
+        raise TaskStartFailure(self.description)
+
+    async def run(self):
+        try:
+            await self.start_until_success()
+        except Exception as e:
+            logging.info(f"{self.name}: Could not run: {e}")
+            # TODO: open
+            pass
+
+        await self.wait()
+
+        # TODO: Maybe do better than an endless loop
+        while True:
+            await asyncio.sleep(1)
+
+
+class TaskStartFailure(Exception):
+    """Task could not be started"""
+
+    def __init__(self, task: TaskDescription):
+        super().__init__(self, f"could not start `{task.command}`")
+
+
+class TaskMaster:
+    tasks: dict[str, Task]
+    configuration: Configuration
+
+    def __init__(self, configuration: Configuration):
+        self.configuration = configuration
+        self.tasks = {}
+
+    async def run(self):
+        logging.info("Starting taskmaster")
+        self.tasks = {
+            name: Task(name, description)
+            for name, description in self.configuration.tasks.items()
+        }
+        try:
+            await asyncio.gather(*[task.run() for task in self.tasks.values()])
+        except asyncio.CancelledError as e:
+            # TODO: graceful shutdown
+            logging.info(f"Stopping {e}...")
+        except KeyboardInterrupt as e:
+            # TODO: graceful shutdown
+            logging.info(f"Stopping {e}...")
